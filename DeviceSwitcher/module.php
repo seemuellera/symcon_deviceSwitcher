96551eec8e44310cf4bf7febbd35355ab7544554
Andreas Seemueller <andreas.seemueller@gmail.com>
2021-04-09 16:39:55 +0200

    Added Backoff Mechanisms for switching

--- DeviceSwitcher/module.php
+++ DeviceSwitcher/module.php
@@ -111,9 +111,43 @@ class DeviceSwitcher extends IPSModule {
 		if (GetValue($this->ReadPropertyInteger("TargetStatusVariable")) != $sourceValue) {
 			
 			$this->LogMessage("Switching Target Device","DEBUG");
-			RequestAction($this->ReadPropertyInteger("TargetStatusVariable"), $sourceValue);
+			$this->RequestActionWithBackOff($this->ReadPropertyInteger("TargetStatusVariable"), $sourceValue);
 		}
 		
+	}
+	
+	protected function RequestActionWithBackOff($variable, $value) {
+		
+		$retries = 4;
+		$baseWait = 100;
+		
+		for ($i = 0; $i <= $retries; $i++) {
+			
+			$wait = $baseWait * $i;
+			
+			if ($wait > 0) {
+				
+				$this->LogMessage("Waiting for $wait milliseconds, retry $i of $retries", "DEBUG");
+				IPS_Sleep($wait);
+			}
+			
+			$result = RequestAction($variable, $value);
+			
+			// Return success if executed successfully
+			if ($result) {
+				
+				return true;
+			}
+			else {
+				
+				$this->LogMessage("Switching Variable $variable to Value $value failed, but will be retried", "WARN");
+			}
+			
+		}
+		
+		// return false as switching was not possible after all these times
+		$this->LogMessage("Switching Variable $variable to Value $value failed after $retries retries. Aborting", "CRIT");
+		return false;
 	}
 
 	public function RequestAction($Ident, $Value) {
